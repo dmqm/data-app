7841508500b8bac2daea9032dce53c319feff896
dmqm <978761353@qq.com>
2019-03-11 14:29:58 +0800

    11

--- data-elasticsearch/src/main/java/com/ycwl/qiny/data/elasticsearch/Application.java
+++ data-elasticsearch/src/main/java/com/ycwl/qiny/data/elasticsearch/Application.java
@@ -1,53 +1,70 @@
 package com.ycwl.qiny.data.elasticsearch;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.ElasticsearchException;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class Application {
-    private static String directoryPath = "D:\\Documents\\2012-4-1";
+    private static String directoryPath = "C:\\Users\\Qiny\\Documents\\Tencent Files\\978761353\\FileRecv\\2012-4-1";
+    private static Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        RestClient restClient = RestClient.builder(
-                new HttpHost("121.49.107.155", 10192, "http")).build();
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("localhost", 9200, "http"),
-                        new HttpHost("localhost", 9201, "http")));
-        List<String> files = getAllFile(directoryPath, false);
+                        new HttpHost("localhost", 10192, "http")));
+        log.info("client test result is : " + clientTest(client));
+        List<String> files = DataUtil.getAllFile(directoryPath, false);
+        for (String file : files) {
+            List<Map> list = DataUtil.getDataFromExcel(file);
+            Map<String, Object> source = new LinkedHashMap<>();
+            list.forEach(map -> {
+                source.put("application_id", 123);
+                source.put("application_name", "electric-app");
+                IndexRequest request = new IndexRequest("java-rest-test", "doc")
+                        .source(map);
+                IndexResponse response = null;
+                try {
+                    response = client.index(request, RequestOptions.DEFAULT);
+                    log.info(response.toString());
+                } catch (ElasticsearchException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
     }
 
-    /**
-     * 获取路径下的所有文件/文件夹
-     *
-     * @param directoryPath  需要遍历的文件夹路径
-     * @param isAddDirectory 是否将子文件夹的路径也添加到list集合中
-     * @return
-     */
-    public static List<String> getAllFile(String directoryPath, boolean isAddDirectory) {
-        List<String> list = new ArrayList<String>();
-        File baseFile = new File(directoryPath);
-        if (baseFile.isFile() || !baseFile.exists()) {
-            return list;
+    public static boolean clientTest(RestHighLevelClient client) {
+        GetRequest getRequest = new GetRequest(
+                "posts",
+                "doc",
+                "1");
+        getRequest.fetchSourceContext(new FetchSourceContext(false));
+        getRequest.storedFields("_none_");
+        boolean exists = false;
+        try {
+            exists = client.exists(getRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        File[] files = baseFile.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                if (isAddDirectory) {
-                    list.add(file.getAbsolutePath());
-                }
-                list.addAll(getAllFile(file.getAbsolutePath(), isAddDirectory));
-            } else {
-                list.add(file.getAbsolutePath());
-            }
-        }
-        return list;
+        return exists;
     }
 }
---
+++ data-elasticsearch/src/main/java/com/ycwl/qiny/data/elasticsearch/DataUtil.java
@@ -0,0 +1,97 @@
+package com.ycwl.qiny.data.elasticsearch;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DataUtil {
+
+    /**
+     * 获取路径下的所有文件/文件夹
+     *
+     * @param directoryPath  需要遍历的文件夹路径
+     * @param isAddDirectory 是否将子文件夹的路径也添加到list集合中
+     * @return
+     */
+    public static List<String> getAllFile(String directoryPath, boolean isAddDirectory) {
+        List<String> list = new ArrayList<>();
+        File baseFile = new File(directoryPath);
+        if (baseFile.isFile() || !baseFile.exists()) {
+            return list;
+        }
+        File[] files = baseFile.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                if (isAddDirectory) {
+                    list.add(file.getAbsolutePath());
+                }
+                list.addAll(getAllFile(file.getAbsolutePath(), isAddDirectory));
+            } else {
+                list.add(file.getAbsolutePath());
+            }
+        }
+        return list;
+    }
+
+    public static List<Map> getDataFromExcel(String filePath) {
+        List<Map> list=new ArrayList<>();
+        if (!filePath.endsWith(".xls") && !filePath.endsWith(".xlsx")) {
+            System.out.println("文件不是excel类型");
+        }
+        FileInputStream fis = null;
+        Workbook wookbook = null;
+        try {
+            //获取一个绝对地址的流
+            fis = new FileInputStream(filePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            //2003版本的excel，用.xls结尾
+            wookbook = new HSSFWorkbook(fis);//得到工作簿
+        } catch (Exception ex) {
+            //ex.printStackTrace();
+            try {
+                //2007版本的excel，用.xlsx结尾
+
+                wookbook = new XSSFWorkbook(fis);//得到工作簿
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        //得到一个工作表
+        Sheet sheet = wookbook.getSheetAt(0);
+        //获得表头
+        Row rowHead = sheet.getRow(0);
+        //判断表头是否正确
+        if (rowHead.getPhysicalNumberOfCells() != 4) {
+            System.out.println("表头的数量不对!");
+            return list;
+        }
+        //获得数据的总行数
+        int totalRowNum = sheet.getLastRowNum();
+        Cell cell;
+        String[] key={"room_id","ammeter","power","time"};
+        //获得所有数据
+        for (int i = 1; i <= totalRowNum; i++) {
+            Row row = sheet.getRow(i);
+            Map map=new LinkedHashMap<String,String>();
+            for(short j=0;j<4;j++){
+                cell = row.getCell(j);
+                cell.setCellType(CellType.STRING);
+                map.put(key[j],cell.getStringCellValue());
+            }
+            list.add(map);
+        }
+        return list;
+    }
+}
